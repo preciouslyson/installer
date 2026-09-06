3bca933f01e8f3a105a1c694975eacbd1554833c
Precious Lyson <precious.lyson@gmail.com>
2026-09-06 10:05:31 +0200

    update installable versions

--- src/VersionManager.php
+++ src/VersionManager.php
@@ -11,7 +11,11 @@ class VersionManager {
     {
         return [
             'minimum' => '^2.2.3', // minimum installable machinjiri version
-            'installable' => []
+            'installable' => [
+                '2.2.3' => "machinjiri v2.2.3",
+                '2.2.4' => "machinjiri v2.2.4",
+                '2.2.5' => "machinjiri v2.2.5",
+            ]
         ];
     }
 
